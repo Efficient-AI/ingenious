53c53837d0418167a80333568fe02cb22eebba53
kowndinya.renduchintala <rharisrikowndinya333@gmail.com>
2022-02-15 17:35:07 +0530

    update prepare_sample.py

--- prepare_sample.py
+++ prepare_sample.py
@@ -1,5 +1,6 @@
+import datasets
 from datasets import load_dataset
 
 dataset=load_dataset("wikitext", "wikitext-2-raw-v1", split='train')
-dataset=dataset.DatasetDict({"train": dataset})
+dataset=datasets.DatasetDict({"train": dataset})
 dataset.save_to_disk("./wikitext-2-raw-v1")
