995f3d22fccc6e7573e756fe0fde9a3fa98486ce
kowndinya-renduchintala <rharisrikowndinya333@gmail.com>
2023-05-11 15:21:52 +0000

    updated code for ingenious

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# LM-pretraining
-## Efficient Pretraining of Language Models
+# INGENIOUS
+## Data Efficient Pre-training of Large Language Models
 ### Environment Setup
 #### Run the following in a sequence to set up the environment for running the code. (It is assumed that you have anaconda installed)
 >- `conda create --name ingenious python=3.9`
@@ -13,19 +13,14 @@
 >- `cd ..`
 >- `conda deactivate`
 #### Configuring the accelerate library according to the training environment
-Run `accelerate config` and answer the following questions
+Run `accelerate config` and answer the questions that follow.
 An example is given below
 - In which compute environment are you running? ([0] This machine, [1] AWS (Amazon SageMaker)): **0**
 - Which type of machine are you using? ([0] No distributed training, [1] multi-CPU, [2] multi-GPU, [3] TPU): **2**
 - How many different machines will you use (use more than 1 for multi-node training)? [1]: **1**
 - Do you want to use DeepSpeed? [yes/NO]: **NO**
-- How many processes in total will you use? [1]: **4**
-- Do you wish to use FP16 (mixed precision)? [yes/NO]: **yes**
+- How many processes in total will you use? [1]: **8**
+- Do you wish to use FP16 (mixed precision)? [yes/NO]: **NO**
 
 ### Running the Code
-Change appropriate parameters in `train_BERT.py` and run it. 
-
-#### There are different scripts that execute different algorithms
-- `run_language_modeling.py` runs traditional BERT pretraining, exactly similar to the Google's BERT paper
-- `run_lm_with_subsets.py` runs the following algorithm: After every `select_every` steps, embeddings are computed for the full dataset, Using the submodular function mentioned as `selection_strategy`, a new subset is selected and this is used to train the model for next `select_every` steps. This process continues till `max_train_steps` training steps are completed
-- `run_lm_with_subsets_knn.py` runs the following algorithm: An initial warmstart is done on the ground set. Assuming that the embeddings computed are not that bad to compute similarities and hence to measure redundancies, we use the computed embeddings at this point to select a subset using the submodular function mentioned as `selection_strategy`. After every 
+Change appropriate parameters in `train_mlm_nsp.py` and run it as - `python3 train_mlm_nsp.py`  to train the model using INGENIOUS.
---
+++ train_mlm_nsp.py
@@ -0,0 +1,76 @@
+import os
+import subprocess
+from datetime import datetime
+import argparse
+
+def parse_args():
+    parser=argparse.ArgumentParser(description="pre- BERT")
+    parser.add_argument(
+        "--visible_gpus",
+        type=str,
+        default="0,1,2,3,4,5,6,7",
+        help="visible_gpus"
+    )
+    parser.add_argument(
+        "--main_process_port",
+        type=int,
+        default=55555,
+        help="main process port for accelerate launch"
+    )
+    args=parser.parse_args()
+    return args
+
+def main():
+    args=parse_args()
+    now=datetime.now()
+    timestamp=now.strftime("%d_%m_%Y_%H:%M:%S")
+    log_dir="/data-mount-hdd/logs/bert_ingenious_"+timestamp+"/"
+    model_dir="/data-mount-hdd/models/bert_ingenious_"+timestamp +"/"
+    subset_dir="/data-mount-hdd/subsets/bert_ingenious_"+timestamp+"/"
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.visible_gpus
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(model_dir, exist_ok=True)
+    os.makedirs(subset_dir, exist_ok=True)
+    l=[
+        "accelerate", "launch", "--main_process_port", f"{args.main_process_port}", "run_mlm_nsp_ingenious.py",
+        "--preprocessed",
+        "--log_dir", log_dir,
+        "--subset_dir", subset_dir,
+        "--load_data_from_disk",
+        "--data_directory", "./bert_dataset_prepared",
+        "--tokenizer_name", "bert-base-uncased",
+        "--preprocess_batch_size", "1000",
+        "--per_device_train_batch_size", "128",
+        "--per_device_eval_batch_size", "128",
+        "--learning_rate", "1e-4",
+        "--lr_max_steps", "1000000",
+        "--weight_decay" ,"0.01",
+        "--max_train_steps", "1000000",
+        "--gradient_accumulation_steps", "1",
+        "--num_warmup_steps", "10000",
+        "--output_dir", model_dir,
+        "--seed", "23",
+        "--max_seq_length", "128",
+        "--preprocessing_num_workers", "96",
+        "--mlm_probability" ,"0.15",
+        "--short_seq_prob", "0.1",
+        "--nsp_probability", "0.5",
+        "--subset_fraction", "0.25",
+        "--selection_strategy", "fl",
+        "--optimizer", "LazyGreedy",
+        "--select_every", "25000",
+        "--partition_strategy", "random",
+        "--layer_for_similarity_computation", "9",
+        "--num_partitions", "1500",
+        "--parallel_processes", "96",
+        "--num_warmstart_epochs", "2",
+        "--checkpointing_steps", "25000",
+        "--temperature", "1.0"
+    ]
+    with open(os.path.join(log_dir, "parameters.txt"), "w") as f:
+        for i in l:
+            f.write(f"{i}\n")
+    subprocess.run(l)
+
+if __name__=="__main__":
+    main()
